c8f1f91fbc3696c9cc82d773d92d4f433b445ca0
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-05-12 02:10:37 +0600

    generate token if the user is real or authentic then set that token to client

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+require('dotenv').config();
+const jwt = require('jsonwebtoken') // step 1
 const port = process.env.PORT || 5000;
 
 
@@ -10,11 +12,40 @@ app.use(cors())
 app.use(express.json())
 
 
+// JWT
+/*
+const jwt = require('jsonwebtoken');
+const accessToken = jwt.sign({accessInfo}, secretCode, {expireInfo});
+
+*/
+
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
+})
+
+app.post('/login', (req, res) => {
+    const user = req.body;
+    if(user.email === 'diptasaha.lpu.cse@gmail.com' && user.pass == '1234567'){
+        const accessToken = jwt.sign({email: user.email}, process.env.ACCESS_TOKEN,{expiresIn: "1h"} );
+        res.send(
+            {
+                success: true,
+                accessToken
+            }
+        )
+
+    }
+    else{
+        res.send({ success: false })
+
+    }
+    
+    
+
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
